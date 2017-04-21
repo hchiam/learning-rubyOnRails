cb63c0613520cc3eec636490d079c1b8aa2a596f
hchiam <hchiam@gmail.com>
2017-04-20 23:26:59 -0400

    add shebang

--- ruby/filename.rb
+++ ruby/filename.rb
@@ -1,3 +1,4 @@
+#!/usr/bin/ruby
 print "Hello world!\n" # double quote char -> replaces \n with newline character
 print 'Hello world!\n' # single quote char -> DOES NOT replace \n with newline
 print "\n"
