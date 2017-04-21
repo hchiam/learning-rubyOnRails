ec45f06b15af1dbffadf4efaeda57bdaf8c5956b
hchiam <hchiam@gmail.com>
2017-04-20 23:44:56 -0400

    add tutorialspoint link

--- ruby/moreExamples.rb
+++ ruby/moreExamples.rb
@@ -1,4 +1,7 @@
 #!/usr/bin/ruby
+
+# see https://www.tutorialspoint.com/ruby/ruby_quick_guide.htm
+
 print "Hello world!\n" # double quote char -> replaces \n with newline character
 print 'Hello world!\n' # single quote char -> DOES NOT replace \n with newline
 print "\n"
